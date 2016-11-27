ce2045989a0082e43b4dcbbcb4c482ac9e2e9069
Zygimantas Benetis <zygimantasdev@gmail.com>
2016-11-27 22:55:32 +0200

    removed inside click

--- src/app/slides/slide.component.ts
+++ src/app/slides/slide.component.ts
@@ -7,10 +7,4 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class SlideComponent {
   @Input() title: string = '';
   @Input() content: string = '';
-  @Output() clicked: EventEmitter<boolean> = new EventEmitter<boolean>();
-
-  public insideButtonClicked() {
-    this.clicked.emit(true);
-  }
-
 }
--- src/app/slides/slides.component.ts
+++ src/app/slides/slides.component.ts
@@ -26,8 +26,4 @@ export class SlidesComponent {
     }
   }
 
-  public slideInsideClicked(data) {
-    alert(data);
-  }
-
 }
